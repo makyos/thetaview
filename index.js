2924040f3926d3cfc336cd1af188de3a93601281
makyos <makyos@gmail.com>
2018-02-23 18:41:10 +0900

    4

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var skyEl = document.getElementById("box");
 
-skyEl.addEventListener('click', function(evt) {
-	console.log('evt', evt);
+skyEl.addEventListener('click', function() {
+	var animation = document.createElement('a-animation');
+	animation.setAttribute('attribute', 'scale');
+	animation.setAttribute('to', '2 2 2');
+	animation.setAttribute('dur', '500');
+	this.appendChild(animation);
+	console.log('evt');
 });
 
