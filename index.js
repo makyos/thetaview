b57df2b5d498f3d33ea2d7d633060c592ae0271e
makyos <makyos@gmail.com>
2018-02-25 22:01:18 +0900

    6

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ box1EL.addEventListener('click', function() {
 	console.log('click! box1');
 });
 
+var box2EL = document.getElementById("box2");
+box2EL.addEventListener('click', function () {
+	console.log('click! box2');
+});
+
 var skyEL = document.getElementById("sky");
 skyEL.addEventListener('click', function () {
 	console.log('click! sky');
